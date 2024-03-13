d7bd8dba4af36daa05f462f0d4fabfbc57b9b569
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-13 21:21:58 +0200

    Create MyComp.tsx

---
+++ src/app/components/MyComp.tsx
@@ -0,0 +1,13 @@
+const MyComp = async () => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+
+//   const data = await res.json();
+//   console.log("MyComp  data:", data);
+  return (
+    <>
+      <h1>HELLO</h1>
+    </>
+  );
+};
+
+export default MyComp;
