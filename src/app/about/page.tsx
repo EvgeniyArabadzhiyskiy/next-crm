98a5e08a718e99b75f3f66d20d6e4a707fbc9760
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-13 21:57:46 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,35 +1,26 @@
-import TransitionLink from "../components/TransitionLink/TransitionLink";
+'use client';
+
+import { useState } from 'react';
+import TransitionLink from '../components/TransitionLink/TransitionLink';
+import Modal from '../components/Modal/Modal';
 
 export default function About() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <main className="bg-white text-black w-screen h-screen flex flex-col items-center justify-center gap-[50px]">
+    <div className="bg-white text-black w-screen h-screen flex flex-col items-center justify-center gap-[50px]">
       <h1 className="text-5xl">ABOUT PAGE</h1>
       <TransitionLink href="/" label="Home ->" />
-    </main>
+
+      <button className="text-2xl" onClick={() => setIsOpen(true)}>
+        Open Modal
+      </button>
+
+      {isOpen && (
+        <Modal setIsOpen={setIsOpen}>
+          <h1 className="text-3xl">Modal Window</h1>
+        </Modal>
+      )}
+    </div>
   );
 }
-
-// "use client";
-
-// import { useRef } from "react";
-
-// import gsap from "gsap"; // <-- import GSAP
-// import { useGSAP } from "@gsap/react"; // <-- import the hook from our React package
-
-// gsap.registerPlugin(useGSAP);
-
-// export default function App() {
-//   const container = useRef<HTMLDivElement>(null);
-
-//   useGSAP(() => {
-//     // gsap code here...
-//     gsap.to("#box", {rotation: 180}); // <-- automatically reverted
-
-//   }, { scope: container }) // <-- scope for selector text (optional)
-
-//  return (
-//    <div ref={container} className="app">
-//      <div id="box"  className="flex w-96 h-96 bg-red-500 font-semibold text-3xl">Hello</div>
-//    </div>
-//  );
-// }
