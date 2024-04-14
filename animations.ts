73080e50fbeda96d08503a3fac9f2de8de947fa2
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-14 20:48:09 +0300

    Update animations.ts

--- animations.ts
+++ animations.ts
@@ -21,31 +21,31 @@ export const animatePageOut = (href: string, router: AppRouterInstance, timeLine
 //   if (animationWrapper) {
     const tl = gsap.timeline();
 
-    tl.to('#transition-element', {opacity: 1, pointerEvents: "auto"})
+    // tl.to('#transition-element', {opacity: 1, pointerEvents: "auto"})
 
 
-    // timeLine?.set('#transition-element', {
-    //   xPercent: -100,
-    //   borderTopRightRadius: "50vh",
-    //   borderBottomRightRadius: "50vh",
-    //   borderTopLeftRadius: "0px",
-    //   borderBottomLeftRadius: "0",
-    // }).to('#transition-element', {
-    //   xPercent: 0,
-    //   duration: 0.8,
-    //   onComplete: () => {
-    //     router.push(href);
-    //   },
-    // })
-    // .to(
-    //   '#transition-element',
-    //   {
-    //     borderTopRightRadius: "0",
-    //     borderBottomRightRadius: "0",
-    //     duration: 0.8,
-    //   },
-    //   "<"
-    // );
+    timeLine?.set('#transition-element', {
+      xPercent: -100,
+      borderTopRightRadius: "50vh",
+      borderBottomRightRadius: "50vh",
+      borderTopLeftRadius: "0px",
+      borderBottomLeftRadius: "0",
+    }).to('#transition-element', {
+      xPercent: 0,
+      duration: 0.8,
+      onComplete: () => {
+        router.push(href);
+      },
+    })
+    .to(
+      '#transition-element',
+      {
+        borderTopRightRadius: "0",
+        borderBottomRightRadius: "0",
+        duration: 0.8,
+      },
+      "<"
+    );
    
   // }
 };
