2b264f5f139addb288881fa9cbf4fdb065e650cb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-14 20:48:11 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import TransitionLink from '../components/TransitionLink/TransitionLink';
 import Modal from '../components/Modal/Modal';
+import Link from 'next/link';
 
 export default function About() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,7 +11,8 @@ export default function About() {
   return (
     <div className="bg-white text-black w-screen h-screen flex flex-col items-center justify-center gap-[50px]">
       <h1 className="text-5xl">ABOUT PAGE</h1>
-      <TransitionLink href="/" label="Home ->" />
+      {/* <TransitionLink href="/" label="Home ->" /> */}
+      <Link href="/">Home</Link>
 
       <button className="text-2xl" onClick={() => setIsOpen(true)}>
         Open Modal
